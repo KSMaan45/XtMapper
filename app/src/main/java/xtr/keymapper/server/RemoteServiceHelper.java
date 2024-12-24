5cbc352cbcfd76d969ce8b0d33fc25f68787bd93
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2024-12-24 20:52:00 +0530

    Changes for running as privileged system app without root

--- app/src/main/java/xtr/keymapper/server/RemoteServiceHelper.java
+++ app/src/main/java/xtr/keymapper/server/RemoteServiceHelper.java
@@ -4,6 +4,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -24,7 +25,7 @@ public class RemoteServiceHelper {
     public static boolean useShizuku = false;
 
     public static void pauseKeymap(Context context){
-        RemoteServiceHelper.getInstance(context, service -> {
+        getInstance(context, service -> {
             try {
                 service.pauseMouse();
             } catch (RemoteException e) {
@@ -99,22 +100,57 @@ public class RemoteServiceHelper {
                     .debuggable(BuildConfig.DEBUG)
                     .version(BuildConfig.VERSION_CODE);
         Shizuku.bindUserService(userServiceArgs, connection);
-}
+    }
 
-    public static void getInstance(Context context, RootRemoteServiceCallback callback){
-        getInstance();
-        if (service != null) {
-            if (callback != null) callback.onConnection(service);
+    public static boolean isSystemApp(Context context) {
+        try {
+            String packageName = context.getPackageName();
+            PackageManager pm = context.getPackageManager();
+            ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
+            return (ai.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) != 0;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    
+    private static void getInstanceAsSystemApp(Context context, RootRemoteServiceCallback callback) {
+        if (service == null) {
+            RemoteService mService = new RemoteService(context.getApplicationContext());
+            mService.startedFromShell = true;
+            service = mService;
+        }
+        if (callback != null) callback.onConnection(service);
+    }
+
+    /**
+     * Get instance of remote service
+     * @param context App context
+     * @param callback Callback when instance of remote service is obtained
+     * Check if we are a system app
+     * If system app -> create instance of service in same process
+     * If not -> Try to get instance from xtmapper binder service added with ServiceManager
+     * Or if Shizuku is available try to obtain service instance
+     * Or if root is available try to start server and obtain instance 
+     */
+    public static void getInstance(Context context, RootRemoteServiceCallback callback) {
+        if (isSystemApp(context)) {
+            getInstanceAsSystemApp(context, callback);
         } else {
-            RemoteServiceConnection connection = new RemoteServiceConnection(callback);
-            if (useShizuku) {
-                if (Shizuku.pingBinder() && Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED)
-                    if (callback != null) bindShizukuService(context, connection);
+            getInstance();
+            if (service != null) {
+                if (callback != null) callback.onConnection(service);
             } else {
-                Boolean hasRootAccess = Shell.isAppGrantedRoot();
-                if (hasRootAccess != null) isRootService = hasRootAccess;
-                Intent intent = new Intent(context, RootRemoteService.class);
-                if (callback != null) RootService.bind(intent, connection);
+                RemoteServiceConnection connection = new RemoteServiceConnection(callback);
+                if (useShizuku) {
+                    if (Shizuku.pingBinder() && Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED)
+                        if (callback != null) bindShizukuService(context, connection);
+                } else {
+                    Boolean hasRootAccess = Shell.isAppGrantedRoot();
+                    if (hasRootAccess != null) isRootService = hasRootAccess;
+                    Intent intent = new Intent(context, RootRemoteService.class);
+                    if (callback != null) RootService.bind(intent, connection);
+                }
             }
         }
     }
